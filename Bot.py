1c8f5e03f25168df5da57ffb1e4d13ddb487785a
Samariii <82685243+Samariii@users.noreply.github.com>
2021-06-11 16:29:28 -0400

    Updated the code and added a details.json to facilitate sign in information

--- Bot.py
+++ Bot.py
@@ -1,49 +1,83 @@
-import time
+# Importing selenium webdriver and selenium wait
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+# Importing the json module to interact with details.json
+import json
 
-# grab the webdriver from the local filesystem
+# Grab the webdriver from the local filesystem
+# Load the json to grab details
 browser = webdriver.Chrome('chromedriver.exe')
+j = open('details.json')
+data = json.load(j)
 
+# Send the webdriver to the intended website
+browser.get(data['link'])
 
-# Send the webdriver var to the intended website
-browser.get('https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440')
+# Setup the loops for the add to cart button
+completeOrder = False
 
-# Test page
-# browser.get('https://www.bestbuy.com/site/ibuypower-trace-mr-gaming-desktop-amd-ryzen-5-3600-8gb-memory-nvidia-geforce-gt-710-2gb-240gb-ssd/6463678.p?skuId=6463678')
-
-complete = False
-
-while not complete:
+while not completeOrder:
+	# Attempt to find the addTC button
 	try:
-		atcbtn = browser.find_element_by_class_name('btn-disabled')
-		browser.refresh()
+		addTC = WebDriverWait(browser, 10).until(
+			EC.element_to_be_clickable(By.CSS_SELECTOR, '.add-to-cart-button')
+		)
 	except:
-		atcbtn = browser.find_element_by_class_name('btn-primary')
-		atcbtn.click()
+		browser.refresh()
+		continue
 
-print("Got to cart")
-browser.get('https://www.bestbuy.com/cart')
+	print("Got the add to cart button")
 
+	try:
+		# Try to click the button and go to cart
+		addTC.click()
+		browser.get("https://www.bestbuy.com/cart")
 
-time.sleep(2)
-checkoutButton = browser.find_element_by_xpath('//*[@id="cartApp"]/div[2]/div[1]/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button')
+		checkout = WebDriverWait(browser, 10).until(
+			EC.presence_of_all_elements_located(By.XPATH, "/html/body/div[1]/main/div/div[2]/div[1]/div/div/span/div/div[2]/div[1]/section[2]/div/div/div[3]/div/div[1]/button")
+		)
+		checkout.click()
 
-print("Checkout imminent")
-checkoutButton.click()
+		# Redirected to the sign in page and logging in
+		print("Got it in the cart, potentially")
 
-time.sleep(2)
-emailBox = browser.find_element_by_xpath('//*[@id="fld-e"]')
+		email = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "fld-e"))
+        )
+		email.send_keys(data['email'])
 
-time.sleep(1)
-passBox = browser.find_element_by_xpath('//*[@id="fld-p1"]')
+		password = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "fld-p1"))
+        )
+		password.send_keys(data['password'])
 
-print("Email entered")
-emailBox.send_keys("samarid1010@gmail.com")
+		signin = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/section/main/div[1]/div/div/div/div/form/div[3]/button"))
+        )
+		signin.click()
+		print("Signing you in..")
 
-print("Password through")
-passBox.send_keys("Samari225!")
+		# Entering the cvv #
+		cvv = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.ID, "credit-card-cvv"))
+        )
+		cvv.send_keys(data['cvv'])
 
-signBox = browser.find_element_by_class_name('btn-lg')
+		# Clicking the place order button
+		placeOrder = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, ".button__fast-track"))
+        )
+		placeOrder.click()
 
-print("Signing in")
-signBox.click()
+		completeOrder = True
+
+	except:
+		# Just incase we get caught up in the checkout
+		# Go back to the link and try again if not in the cart
+		browser.get(data['link'])
+		print("Not in the cart go back!!")
+		continue
+
+print("You got it!")
